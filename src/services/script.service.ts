909d6c0a71dbf767b05d7bff1646142d2fa5e135
Kevin Phelps <KevinPhelps11@gmail.com>
2017-07-12 14:27:40 -0500

    feat: better output from generated script

--- src/services/script.service.ts
+++ src/services/script.service.ts
@@ -16,6 +16,8 @@ export class ScriptService {
       this.projectService.getProjects()
         .then(projects => this.dependencyGraphService.orderProjectsByDependencyGraph(projects))
         .then(projects => {
+          console.log('echo off');
+
           for (const taskName of taskNames) {
             this.generateTaskScript(taskName, projects);
           }
@@ -24,6 +26,7 @@ export class ScriptService {
 goto success
 
 :error
+echo ${this.colorEcho('There was an error in the above task. Exiting...', 31)}
 exit /b 1
 
 :success
@@ -54,11 +57,20 @@ exit /b`);
         }
 
         console.log(`
+echo;
+echo ${this.colorEcho(`*** Running task "${taskName} in project "${project.name}" (${cwd}). ***`, 32)}
+echo;
+echo ${this.colorEcho(`${cwd}^> ${command.command}`, 90)}
 pushd ${cwd}
 call ${command.command}
 if errorlevel 1 goto error
 popd`);
       }
     }
+  }
+
+  private colorEcho(message: string, color: number) {
+    // color code reference: https://gist.github.com/mlocati/fdabcaeb8071d5c75a2d51712db24011#file-win10colors-cmd
+    return `[${color}m${message}[0m`;
   }
 }
