800b518e1884088168b32eeceaaabd0baa62a70d
Kevin Phelps <KevinPhelps11@gmail.com>
2017-07-21 04:11:51 -0500

    refactor (web): use list to get build configurations

--- src/web/app/shared/services/settings.service.ts
+++ src/web/app/shared/services/settings.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 
-import { mapToArray, SimpleMap } from './../../../../common/helpers/object.helpers';
 import { BuildConfiguration } from './../../../../common/interfaces/build-configuration';
 import { DatabaseService } from './database.service';
 
@@ -9,7 +8,6 @@ export class SettingsService {
   constructor(private database: DatabaseService) { }
 
   getBuildConfigurations() {
-    return this.database.object<SimpleMap<BuildConfiguration>>('build-configurations')
-      .map(buildConfigurationMap => mapToArray(buildConfigurationMap));
+    return this.database.list<BuildConfiguration>('build-configurations');
   }
 }
