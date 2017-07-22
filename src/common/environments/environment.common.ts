b7320e1212075513da93a04fbd0239713505bc84
Kevin Phelps <KevinPhelps11@gmail.com>
2017-07-22 01:19:37 -0500

    feat: update dependencies
    json-loader 0.5.4 -> 0.5.5 has a breaking change.

--- src/common/environments/environment.common.ts
+++ src/common/environments/environment.common.ts
@@ -3,6 +3,6 @@ const node = typeof window === 'undefined';
 export const commonEnvironment = {
   node,
   browser: node === false,
-  version: require('./../../../package.json').version,
+  version: require('./../../../package.json').default.version,
   serverPort: node ? process.env.PORT || 5000 : undefined
 };
